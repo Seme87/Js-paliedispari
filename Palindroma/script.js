d33f88452cf649c64b98b7ebf52d321224e5ea9b
Seme87 <massimomonteleone@hotmail.com>
2022-10-28 16:24:26 +0200

    funzione palindroma

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,17 +1,21 @@
 // Chiedere all’utente di inserire una parola
 const word= prompt("inserisci una parola!")
 
-
-
 let result=false;
 // Creare una funzione per capire se la parola inserita è palindroma
-for(let i = word.length-1; i >= 0; i--){
-    console.log(word[i])
-    if(word[i] === word){
-        result=true
-    }
-    console.log(result)
-}
 
+  for(let i = word.length-1; i >= 0; i--){
+
+    console.log(word[i], word);
 
     
+    const wordCont =`${word[i]}`
+    console.log(wordCont);
+
+
+    /* if(word[i]===word){
+        result=true;
+    } */
+
+
+  }  
